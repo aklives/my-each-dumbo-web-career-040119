87af18abb503370890d3bd7c0fe81be4794a9c4e
Adam Kearney <aklives@hotmail.com>
2019-03-16 09:48:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,12 @@
-collection = [1, 2, 3, 4]
-new_collection = []
 def my_each(array)
   i = 0
   while i < array.length
     yield array[i]
     i = i + 1
   end
-new_collection
+  array
 end
 
 my_each(collection) do |i|
-  new_collection << i
+  puts i
 end
